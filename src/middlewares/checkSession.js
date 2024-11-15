5e957732ec5b8677a5db06b93cff4f56b34f1ecd
tsukuyomi09 <samuelesala.business@gmail.com>
2024-11-16 04:23:49 +0800

    complete change structure files - switched from pure node.js to express

--- src/middlewares/checkSession.js
+++ src/middlewares/checkSession.js
@@ -1,33 +1,30 @@
-const { client } = require("./database");
+const { client } = require('../database/db'); 
 
 const checkSession = (req, res, next) => {
+    // Estrai il sessionId dai cookies
     const cookies = req.headers.cookie;
     const sessionId = cookies && cookies.split('; ').find(c => c.startsWith('sessionId=')).split('=')[1];
 
     if (!sessionId) {
-        res.writeHead(401); // 400 Bad Request
-        res.end("Sessione non valida o non presente.");
-        return;
+        return res.status(401).json({ message: "Sessione non valida o non presente." });
     }
 
+    // Query per verificare la sessione nel database
     const sessionQuery = "SELECT user_id FROM sessions WHERE session_id = $1";
 
     client.query(sessionQuery, [sessionId])
         .then(result => {
             if (result.rows.length === 0) {
-                res.writeHead(404, { "Content-Type": "application/json" }); // 404 Not Found
-                res.end(JSON.stringify({ message: "Nessun oggetto trovato per questo utente" }));
-                return;
+                return res.status(404).json({ message: "Sessione non trovata per questo utente." });
             }
 
             // Aggiungi l'user_id alla richiesta
             req.user_id = result.rows[0].user_id;
-            next(); // Continua la richiesta alla rotta successiva
+            next(); // Continua con la richiesta alla rotta successiva
         })
         .catch(err => {
             console.error("Errore durante il recupero della sessione:", err);
-            res.writeHead(500);
-            res.end("Errore del server");
+            return res.status(500).json({ message: "Errore del server" });
         });
 };
 
---
+++ src/routes/playersQueue.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { client } = require('../database/db'); 
+const checkSession = require('../middlewares/checkSession');
+
+
+router.get('/gamequeue/:game_id/players', checkSession,  async (req, res) => {
+    const { game_id } = req.params;  // Ottieni il game_id dal parametro della rotta
+
+    const query = `
+        SELECT u.username
+        FROM users u
+        JOIN players_in_game p ON u.user_id = p.user_id
+        WHERE p.game_id = $1
+        ORDER BY p.turn_order;`;
+
+    try {
+        const result = await client.query(query, [game_id]);  // Esegui la query
+        res.status(200).json(result.rows);  // Restituisci i dati come JSON
+    } catch (err) {
+        console.error('Errore nella query:', err);
+        res.status(500).json({ error: 'Errore nella query' });  // Gestione errore
+    }
+})
+
+module.exports = router;
