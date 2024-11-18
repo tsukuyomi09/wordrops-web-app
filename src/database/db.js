916205cdf182602262ff29f76a860a5b976a72be
tsukuyomi09 <samuelesala.business@gmail.com>
2024-11-19 01:04:19 +0800

    test api railway

--- src/database/db.js
+++ src/database/db.js
@@ -11,7 +11,10 @@ const connectDB = async () => {
     try {
         await client.connect();
         console.log('Connesso al database railway');
+        const res = await client.query('SELECT NOW()');
+        console.log('Data e ora dal database:', res.rows[0]);
     } catch (err) {
+        
         console.error('Errore di connessione:', err.stack);
     }
 };
