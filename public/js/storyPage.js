81188fbf6848696928e8fe8c61b2b0933ba6a0b5
tsukuyomi09 <samuelesala.business@gmail.com>
2025-05-14 23:20:52 +0200

    page libro: added toggle author and chapters change text color

--- public/js/storyPage.js
+++ public/js/storyPage.js
@@ -1,15 +1,26 @@
+/// MENU TOGGLE CHAPTERS AND AUTHORS ///
+
 const btnAutori = document.getElementById("btnAutori");
 const dropAutori = document.getElementById("dropupAutori");
-
 const btnCapitoli = document.getElementById("btnCapitoli");
 const dropCapitoli = document.getElementById("dropupCapitoli");
 
-btnAutori.addEventListener("click", () => {
+function authorsDropup() {
+    btnAutori.classList.toggle("text-custom-blue");
+    btnAutori.classList.toggle("text-gray-800");
+    btnCapitoli.classList.remove("text-custom-blue");
+    btnCapitoli.classList.add("text-gray-800");
     dropAutori.classList.toggle("hidden");
-    dropCapitoli.classList.add("hidden"); // chiude l’altro se aperto
-});
+    dropCapitoli.classList.add("hidden");
+}
 
-btnCapitoli.addEventListener("click", () => {
+function chaptersDropup() {
+    btnCapitoli.classList.toggle("text-custom-blue");
+    btnCapitoli.classList.toggle("text-gray-800");
+    btnAutori.classList.remove("text-custom-blue");
+    btnAutori.classList.add("text-gray-800");
     dropCapitoli.classList.toggle("hidden");
     dropAutori.classList.add("hidden");
-});
+}
+
+/// MENU TOGGLE CHAPTERS AND AUTHORS ///
