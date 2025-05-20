c0ee07f3f1959c533ca3c799e7325d651e5e7ca6
tsukuyomi09 <samuelesala.business@gmail.com>
2025-05-20 18:54:19 +0200

    story data index removed

--- src/routes/story/index.js
+++ src/routes/story/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/story-data", require("./storyData"));
 router.use("/story-rate", require("./storyRate"));
 
 module.exports = router;
